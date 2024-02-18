e49c8152970e4529e20c4a6501735a39a6a4edd8
SARAC <careeralpersarac@gmail.com>
2024-02-18 21:08:13 +0300

    Add project files.

---
+++ UnitTest/DegreeConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTest
+{
+    public class DegreeConverter
+    {
+        public double ToFahrenheit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+        public double ToCelcius(double fahrenheit)
+        {
+            return (32*fahrenheit-32 ) *5/9;
+        }
+    }
+}
