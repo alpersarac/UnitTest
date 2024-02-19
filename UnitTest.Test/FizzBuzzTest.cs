aec00a1006fa560278d945adfc5241f3f4c4ca25
SARAC <careeralpersarac@gmail.com>
2024-02-19 23:22:11 +0300

    FizzBuzz test implementation.

---
+++ UnitTest.Test/FizzBuzzTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Test
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        private FizzBuzz fizzBuzz;
+        [SetUp]
+        public void Setup()
+        {
+            fizzBuzz= new FizzBuzz();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            fizzBuzz = null;
+        }
+        [Test]
+        public void FizzBuzzTest_Returns_FizzBuzz()
+        {
+            var result = FizzBuzz.Ask(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        [Test]
+        public void FizzBuzzTest_Returns_Buzz()
+        {
+            var result = FizzBuzz.Ask(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void FizzBuzzTest_Returns_Fizz()
+        {
+            var result = FizzBuzz.Ask(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+    }
+}
